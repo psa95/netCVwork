7209e9614ce35a05f98cf31e7a596536321f3487
Prasad Acharya <ps274@hw.ac.uk>
2018-07-14 15:13:47 +0400

    Basic navigation setup

---
+++ client/src/views/login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
+
+class Login extends Component {
+  render() {
+    return(
+      <div>
+        LOGIN PAGE
+      </div>
+    );
+  }
+}
+
+export default Login
