c13d174ccb85e98d37c248586da12017e3e9eaf8
saarahhuda <saarahhuda8@gmail.com>
2018-07-26 16:34:34 +0400

    mongo connection

--- client/src/views/login.js
+++ client/src/views/login.js
@@ -5,7 +5,18 @@ class Login extends Component {
   render() {
     return(
       <div>
-        LOGIN PAGE
+          <h1 align="center">LOGIN PAGE</h1>
+          <br/>
+          <br/>
+          <div align="center">
+            <form method="POST" action="/check-login">
+              <label>Username : </label>
+              <input name="username" type="text"/><br/>
+              <label>Password : </label>
+              <input name="password" type="password"/><br/>
+              <button type="submit">Log In</button>
+            </form>
+          </div>
       </div>
     );
   }
