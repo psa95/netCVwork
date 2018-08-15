00e3f362aab57f3571c3ddd709ddcc4fce5da424
saarahhuda <saarahhuda8@gmail.com>
2018-08-15 19:45:01 +0400

    still in progress

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,10 +5,13 @@ class Header extends Component {
     isLoggedIn: false
   }
   onLogout(){
+    console.log('logout btn clicked');
     localStorage.clear();
-    window.location.assign("http://localhost:3000/")
+    window.location.assign("http://localhost:8882/")
   }
+
   render(){
+    console.log("header component");
     return <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <ul className="navbar-nav mr-auto">
         <li className="nav-item">
@@ -16,16 +19,33 @@ class Header extends Component {
         </li>
         <li className="nav-item">
           <NavLink className="nav-link" activeClassName="active" to={'/users/'}>Dashboard</NavLink>
-        </li >
+        </li>
+       {localStorage.getItem('token') ? 
+      <ul className="navbar-nav ml-auto">
         <li className="nav-item">
-          {localStorage.getItem("name")}
+          <a className="nav-link">{localStorage.getItem("name")}</a>
         </li>
         <li className="nav-item">
-        <button onClick={this.onLogout} className="btn btn-danger">Logout</button>
+          <button onClick={this.logout} className="btn btn-warning">Logout</button>
         </li>
+      </ul>  : null }
+
       </ul>
     </nav>
   }
 }
+
+const LogoutBtn = (props) => {
+  return <div>
+  <button onClick={this.onLogout} className="btn btn-danger">Logout</button>  
+  </div>
+}
+
+const DisplayName = (props) => {
+  return <div>
+  <h4>{localStorage.getItem("name")}</h4>
+  </div>
+}
+
 
 export default Header;
