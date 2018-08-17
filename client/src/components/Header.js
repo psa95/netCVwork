ea1068b6607eab6c22b9ef3105cf589b1a1be429
Prasad Acharya <ps274@hw.ac.uk>
2018-08-17 11:17:53 +0400

    Menu added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom'
 import { userLogout, isLoggedIn, userInfo } from '../helpers/authentication';
+import { Menu, Segment } from 'semantic-ui-react'
 class Header extends Component {
+
+  state = { activeItem: 'home' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
   logout = (e) => {
     userLogout();
     window.location.href = "/";
@@ -9,25 +15,27 @@ class Header extends Component {
 
   render(){
     const full_name = isLoggedIn() ? userInfo().full_name : null;
-    return <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <ul className="navbar-nav mr-auto">
-        <li className="nav-item">
-          <NavLink className="nav-link" activeClassName="active" to={'/about'}>About</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" activeClassName="active" to={'/users/'}>Dashboard</NavLink>
-        </li>
-      </ul>
-      {isLoggedIn() ? 
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <a className="nav-link">{ full_name }</a>
-        </li>
-        <li className="nav-item">
-          <button onClick={this.logout} className="btn btn-warning">Logout</button>
-        </li>
-      </ul>  : null }
-    </nav>
+    const { activeItem } = this.state
+    return (
+      <Segment inverted>
+        <Menu inverted pointing secondary>
+          <Menu.Item as={NavLink} to="/about" name='about' />
+            <Menu.Item
+              as={NavLink}
+              to="/users/"
+              name='users'
+            />
+            {isLoggedIn() ?
+              <Menu.Menu position="right">
+                <Menu.Item name={full_name} content={full_name} />
+                <Menu.Item
+                  name='logout'
+                  onClick={this.logout}
+                />
+              </Menu.Menu>  : null }
+        </Menu>
+      </Segment>
+    )
   }
 }
 
